dc1584f2d2efaaf3a13367376e5d0b1c1f8cf383
gabbydiaz <gabbymdiaz@outlook.com>
2020-04-12 02:14:58 -0400

    added formData

--- MedixFinalProject/Classes/FormData.swift
+++ MedixFinalProject/Classes/FormData.swift
@@ -1,13 +1,26 @@
 //
 //  FormData.swift
-//  MedixFinalProject
+//  Medix
 //
-//  Created by Xcode User on 2020-04-12.
+//  Created by Gabby
 //  Copyright © 2020 Xcode User. All rights reserved.
 //
 
 import UIKit
 
 class FormData: NSObject {
-
+    
+    var ID: Int?
+    var username: String?
+    var medname: String?
+    var medquantity: Int?
+    var startdate: String?
+    
+    func initWithFormData(theRow id: Int, theUsername userName: String, theMedName medName: String, theMedQuantity medQuantity: Int, theStartDate startDate: String) {
+        ID = id;
+        username = userName;
+        medname = medName;
+        medquantity = medQuantity;
+        startdate = startDate;
+    }
 }
